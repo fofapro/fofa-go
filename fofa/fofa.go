45156fbc1c2605b1ada5f3ec19bfeaa12e6c3eef
fangsihao <fangsihao@17paipai.cn>
2017-03-19 01:01:37 +0800

    update json encoder

--- fofa/fofa.go
+++ fofa/fofa.go
@@ -53,10 +53,11 @@ type result struct {
 
 // User struct for fofa user
 type User struct {
-	Email  string `json:"email"`
-	Fcoin  int    `json:"fcoin"`
-	Vip    bool   `json:"bool"`
-	Avatar string `json:"avatar"`
+	Email  string `json:"email,omitempty"`
+	Fcoin  int    `json:"fcoin,omitempty"`
+	Vip    bool   `json:"bool,omitempty"`
+	Avatar string `json:"avatar,omitempty"`
+	Err    string `json:"errmsg,omitempty"`
 }
 
 // Results fofa result set
@@ -67,8 +68,8 @@ const (
 )
 
 var (
-	errFofaReplyWrongFormat = errors.New("Fofa Reply With Wrong Format.")
-	errFofaReplyNoData      = errors.New("No Data In Fofa Reply.")
+	errFofaReplyWrongFormat = errors.New("Fofa Reply With Wrong Format")
+	errFofaReplyNoData      = errors.New("No Data In Fofa Reply")
 )
 
 // NewFofaClient create a fofa client
@@ -85,6 +86,21 @@ func NewFofaClient(email, key []byte) *Fofa {
 			Transport: transCfg, // disable tls verify
 		},
 	}
+}
+
+// Get overwrite http.Get
+func (ff *Fofa) Get(u string) ([]byte, error) {
+
+	body, err := ff.Client.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer body.Body.Close()
+	content, err := ioutil.ReadAll(body.Body)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
 }
 
 // QueryAsJSON make a fofa query and return json data as result
@@ -114,165 +130,63 @@ func (ff *Fofa) QueryAsJSON(page uint, args ...[]byte) ([]byte, error) {
 		[]byte("&page="), []byte(strconv.Itoa(int(page))),
 	}, []byte(""))
 	fmt.Printf("%s\n", q)
-	resp, err := ff.Get(string(q))
+	content, err := ff.Get(string(q))
 	if err != nil {
-		log.Errorf("%v\n", err.Error())
 		return nil, err
 	}
-	defer resp.Body.Close()
-	buf, err := ioutil.ReadAll(resp.Body)
-	errmsg, err := jsonparser.GetString(buf, "errmsg")
+	errmsg, err := jsonparser.GetString(content, "errmsg")
 	if err == nil {
 		err = errors.New(errmsg)
 	} else {
 		err = nil
 	}
-	return buf, err
+	return content, err
 }
 
 // QueryAsArray make a fofa query and
 // return array data as result
 // echo 'domain="nosec.org"' | base64 - | xargs -I{}
 // curl "https://fofa.so/api/v1/search/all?email=${FOFA_EMAIL}&key=${FOFA_KEY}&qbase64={}"
-func (ff *Fofa) QueryAsArray(page uint, args ...[]byte) (Results, error) {
+func (ff *Fofa) QueryAsArray(page uint, args ...[]byte) (result Results, err error) {
 
-	var (
-		mapFields   = make(map[string]int)
-		resultArray = [][]byte(nil)
-	)
+	var content []byte
 
-	data, err := ff.QueryAsJSON(page, args...)
+	content, err = ff.QueryAsJSON(page, args...)
 	if err != nil {
-		log.Errorf("err: %v\n", err.Error())
 		return nil, err
 	}
 
-	// map field to index
-	if len(args) > 1 && args[1] != nil {
-		fields := strings.Split(string(args[1]), ",")
-		for i, field := range fields {
-			mapFields[strings.Trim(field, " ")] = i
-		}
-	} else {
-		mapFields["domain"] = 0
-		mapFields["host"] = 1
-		mapFields["ip"] = 2
-		mapFields["port"] = 3
-		mapFields["title"] = 4
-		mapFields["country"] = 5
-		mapFields["city"] = 6
-
-	}
-
-	errmsg, err := jsonparser.GetString(data, "errmsg")
+	errmsg, err := jsonparser.GetString(content, "errmsg")
 	// err equals to nil on error
 	if err == nil {
-		err = errors.New(errmsg)
-		log.Errorf("err: %v\n", errmsg)
-		return nil, err
+		return nil, errors.New(errmsg)
 	}
 
-	results, dataType, _, err := jsonparser.Get(data, "results")
+	err = json.Unmarshal(content, result)
 
-	switch {
-	case dataType != jsonparser.Array:
-		log.Errorf("err: %v\n", err.Error())
-		return nil, err
-	case err != nil:
-		log.Errorf("err: %v\n", err.Error())
-		return nil, err
-	}
-	size, err := jsonparser.GetInt(data, "size")
-	if err != nil {
-		log.Errorf("fofa reply with wrong format.\n%s\n", data)
-		return nil, errFofaReplyWrongFormat
-	}
-	if size < 1 {
-		log.Errorf("no data in fofa reply.\n%s\n", data)
-		return nil, errFofaReplyNoData
-	}
-	if len(mapFields) > 1 {
-		resultArray = bytes.Split(results[2:len(results)-2], []byte("],["))
-	} else {
-		resultArray = bytes.Split(results[1:len(results)-1], []byte{','})
-	}
-	queryArray := make(Results, len(resultArray), len(resultArray))
-	for i, v := range resultArray {
-		tmp := bytes.Split(v, []byte{','})
-
-		if a, ok := mapFields["domain"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].Domain = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["host"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].Host = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["ip"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].IP = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["port"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].Port = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["title"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].Title = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["country"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].Country = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-		if a, ok := mapFields["city"]; ok {
-			if len(tmp[a]) > 2 {
-				queryArray[i].City = string(tmp[a][1 : len(tmp[a])-1])
-			}
-		}
-	}
-	return queryArray, nil
+	return
 }
 
 // UserInfo get user information
 func (ff *Fofa) UserInfo() (user *User, err error) {
 	user = new(User)
 	queryStr := strings.Join([]string{"https://fofa.so/api/v1/info/my?email=", string(ff.email), "&key=", string(ff.key)}, "")
-	resp, e := ff.Get(queryStr)
-	if e != nil {
-		user = nil
-		err = e
-	} else {
-		defer resp.Body.Close()
-		body, e := ioutil.ReadAll(resp.Body)
-		if e != nil {
-			err = e
-		} else {
-			errmsg, e := jsonparser.GetString(body, "errmsg")
-			if e == nil {
-				err = errors.New(errmsg)
-			} else {
-				email, err1 := jsonparser.GetString(body, "email")
-				fcoin, err2 := jsonparser.GetInt(body, "fcoin")
-				vip, err3 := jsonparser.GetBoolean(body, "isvip")
-				avatar, err4 := jsonparser.GetString(body, "avatar")
-				if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
-					err = errors.New("illegal format userinfo")
-				} else {
-					user.Avatar = avatar
-					user.Email = email
-					user.Fcoin = int(fcoin)
-					user.Vip = vip
-				}
-			}
-		}
+
+	content, err := ff.Get(queryStr)
+
+	if err != nil {
+		return nil, err
 	}
-	return
+
+	if err = json.Unmarshal(content, user); err != nil {
+		return nil, err
+	}
+
+	if len(user.Err) != 0 {
+		return nil, errors.New(user.Err)
+	}
+
+	return user, nil
 }
 
 func (u *User) String() string {
